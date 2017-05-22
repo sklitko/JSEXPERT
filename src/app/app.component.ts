8b8d18c493075b956f471cd578298e921cb228ad
klitko <zheltovodets@ukr.net>
2017-05-22 12:06:36 +0300

    firts commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = 'Каталог фильмов JSExpert';
+
+  links = [
+    { path: '/dashboard', icon: 'home', label: 'Главная'},
+    { path: '/filmList', icon: 'event_name', label: 'Все фильмы'},
+    { path: '/profile', icon: 'settings', label: 'Профиль'}
+  ];
+
+}
---
+++ src/app/film-card/film-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {FilmService} from '../film.service'
+
+@Component({
+  selector: 'film-card',
+  templateUrl: './film-card.component.html',
+  styleUrls: ['./film-card.component.css']
+})
+export class FilmCardComponent implements OnInit {
+  @Input()
+  filmId: string; 
+  filmItem: {} = {};
+  constructor(private filmCardService: FilmService) { }
+
+  ngOnInit() {
+    if(!this.filmId) {return;}
+    this.filmCardService.getFilmById(this.filmId).subscribe(data => {
+      this.filmItem = data;
+    })
+  }
+
+}
+
